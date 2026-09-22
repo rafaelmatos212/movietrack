fe6be46e9d74ad262079bd374e9ca2c9fb81a291
Rafael Matos <rafaelmatos212@gmail.com>
2026-09-22 19:34:31 -0300

    [back] fix: corrige leitura de claims do JWT (sub e email vindo vazios)

--- back/MovieTrack.Api/Program.cs
+++ back/MovieTrack.Api/Program.cs
@@ -62,6 +62,7 @@ builder.Services.AddAuthentication(options =>
 })
 .AddJwtBearer(options =>
 {
+    options.MapInboundClaims = false;
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
